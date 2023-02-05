fed36beea1a7f4892575ea69ba9fbc2167d55575
Николай <colt92@inbox.ru>
2023-02-05 19:52:30 +0300

    Initial commit

---
+++ src/main/java/pro/sky/recepts/ReceptsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.recepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/recepts/ReceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.recepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
